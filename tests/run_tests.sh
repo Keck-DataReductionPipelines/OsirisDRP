8441408070feb497b25c3ad1d4f5a81023091601
Alex Rudy <alex.rudy@gmail.com>
2016-05-04 10:08:56 -0700

    Minor improvements to the test runner.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+shopt -s extglob
 wd=`dirname $0`
 idl -IDL_STARTUP "$wd/drpStartup.pro" -e drpTest
-files="$wd/**/expected/*.fits"
+files=$(ls $wd/**/expected/*.fits)
 for file in $files
 do
     outfile=`echo $file | sed -E "s|/expected/|/|"`
+    echo "fitsdiff -c SIMPLE $file $outfile"
     fitsdiff -c SIMPLE $file $outfile
 done
