620f827cadaf6828ef906cf09d4f76a8d12891ba
Alex Rudy <alex.rudy@gmail.com>
2016-05-05 16:22:56 -0700

    Removing comment cards from FITS headers during diff.

--- tests/drptestbones/diff.py
+++ tests/drptestbones/diff.py
@@ -2,7 +2,8 @@
 
 from astropy.extern import six
 from astropy.io.fits.diff import FITSDiff
-
+from astropy.io import fits
+from contextlib import nested
 from astropy.extern.six.moves import StringIO
 
 __all__ = ['fits_osiris_allclose']
@@ -10,15 +11,25 @@ __all__ = ['fits_osiris_allclose']
 def fits_osiris_allclose(a, b):
     """Assert that two OSIRIS fits files are close."""
     
-    report = StringIO()
-    diff = FITSDiff(
-        a, b,
-        ignore_keywords=["COMMENT"],
-        ignore_comments=["SIMPLE"],
-        ignore_fields=[],
-        ignore_blanks=True,
-        ignore_blank_cards=True,
-        tolerance=1e-5)
-    diff.diff_keyword_count = ()
-    diff.report(fileobj=report)
-    assert diff.identical, report.getvalue()
+    a = fits.open(a)
+    b = fits.open(b)
+    
+    try:
+        del a[0].header['COMMENT']
+        del b[0].header['COMMENT']
+    
+        report = StringIO()
+        diff = FITSDiff(
+            a, b,
+            ignore_keywords=["COMMENT"],
+            ignore_comments=["SIMPLE"],
+            ignore_fields=[],
+            ignore_blanks=True,
+            ignore_blank_cards=True,
+            tolerance=1e-5)
+        diff.report(fileobj=report)
+        assert diff.identical, report.getvalue()
+    
+    finally:
+        a.close()
+        b.close()
