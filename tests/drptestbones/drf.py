ec34c314bfff363191f7b23afff2627d54466b03
Alex Rudy <alex.rudy@gmail.com>
2016-05-05 14:02:44 -0700

    Fixing the DRF parser.

--- tests/drptestbones/drf.py
+++ tests/drptestbones/drf.py
@@ -9,14 +9,15 @@ __all__ = ['expand_osiris_path']
 
 def expand_osiris_path(path):
     """Expand an OSIRIS path which might contain environment variables."""
-    path = os.path.normpath(os.path.expanduser(os.path.expandvars(path)))
+    return os.path.normpath(os.path.expanduser(os.path.expandvars(path)))
     
 
 class OsirisDRF(object):
     """A data reduction file for Osiris DRF"""
-    def __init__(self, tree):
+    def __init__(self, tree, expand=True):
         super(OsirisDRF, self).__init__()
         self.tree = tree
+        self.expand = expand
         
     @classmethod
     def parse(cls, filename):
@@ -26,29 +27,44 @@ class OsirisDRF(object):
     @property
     def log_directory(self):
         """Check that the log directory exists."""
-        return expand_osiris_path(self.tree.find("DRF").get('LogPath'))
+        path = self.tree.getroot().get('LogPath')
+        if self.expand:
+            path = expand_osiris_path(path)
+        return path
     
     @property
     def output_directory(self):
         """Output directory"""
-        return expand_osiris_path(self.tree.find("DRF/dataset").get("OutputDir"))
+        path = self.tree.getroot().find("dataset").get("OutputDir")
+        if self.expand:
+            path = expand_osiris_path(path)
+        return path
         
     @property
     def input_directory(self):
         """Input directory."""
-        return expand_osiris_path(self.tree.find("DRF/dataset").get("InputDir"))
+        path = self.tree.getroot().find("dataset").get("InputDir")
+        if self.expand:
+            path = expand_osiris_path(path)
+        return path
     
     def data_files(self):
         """A list of input data files."""
-        dataset = self.tree.find("DRF/dataset")
+        dataset = self.tree.getroot().find("dataset")
         input_directory = dataset.get("InputDir")
-        return [ expand_osiris_path(os.path.join(input_directory, fits.get("FileName")))
+        paths = [ os.path.join(input_directory, fits.get("FileName"))
             for fits in dataset.findall("fits") ]
+        if self.expand:
+            paths = [expand_osiris_path(path) for path in paths ]
+        return paths
         
     def calibration_files(self):
         """A list of input calibration files."""
-        return [ expand_osiris_path(module.get('CalibrationFile'))
-            for module in self.tree.findall("DRF/module") if 'CalibrationFile' in module.attrib ]
+        paths = [ module.get('CalibrationFile')
+            for module in self.tree.getroot().findall("module") if 'CalibrationFile' in module.attrib ]
+        if self.expand:
+            paths = [expand_osiris_path(path) for path in paths ]
+        return paths
         
     
     def files(self):
