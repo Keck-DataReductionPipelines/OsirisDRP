8ee4b4b6f6d436ff7e45709b1e8e41c536369669
Alex Rudy <alex.rudy@gmail.com>
2016-05-05 10:38:58 -0700

    Our first test!

---
+++ tests/test_emission_line/test_emission_line.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+
+from drptestbones.backbone import prepare_and_consume_queue_directory
+from drptestbones.diff import fits_osiris_allclose
+
+def test_emission_line():
+    """Test FITS emission lines"""
+    
+    queue_directory = os.path.dirname(__file__)
+    prepare_and_consume_queue_directory(queue_directory)
+    
+    output_file = os.path.join(queue_directory, "s150531_a025002_Kn5_035.fits")
+    expected_file = os.path.join(queue_directory, "s150531_a025002_Kn5_035_ref.fits")
+    fits_osiris_allclose(output_file, expected_file)
