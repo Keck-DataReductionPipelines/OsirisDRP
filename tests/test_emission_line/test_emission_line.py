0740aaf1b9cc0b69066b9263c69a16145f621a08
Alex Rudy <alex.rudy@gmail.com>
2016-05-05 16:35:47 -0700

    Improve example test layout.

--- tests/test_emission_line/test_emission_line.py
+++ tests/test_emission_line/test_emission_line.py
@@ -8,13 +8,18 @@ from drptestbones.backbone import consume_queue_directory
 from drptestbones.diff import fits_osiris_allclose
 from drptestbones.fetchdata import get_test_file
 
-def test_emission_line(drf_queue):
-    """Test FITS emission lines"""
-    # Download the reference file to compare to.
-    get_test_file('test_emission_line', 's150531_a025002_Kn5_035_ref.fits')
+@pytest.fixture(scope='module')
+def reference_file(request):
+    """Download the reference file."""
+    filename = 's150531_a025002_Kn5_035_ref.fits'
+    get_test_file(request.module.__name__, filename)
+    return filename
     
+
+def test_emission_line(drf_queue, reference_file):
+    """Test FITS emission lines"""
     consume_queue_directory(drf_queue)
     output_file = os.path.join(drf_queue, "s150531_a025002_Kn5_035.fits")
-    expected_file = os.path.join(drf_queue, "s150531_a025002_Kn5_035_ref.fits")
+    expected_file = os.path.join(drf_queue, reference_file)
     fits_osiris_allclose(output_file, expected_file)
 
