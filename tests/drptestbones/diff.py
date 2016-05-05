3dfd1dc8660d54fbcaac737353a105fced1ef1e3
Alex Rudy <alex.rudy@gmail.com>
2016-05-05 10:38:52 -0700

    FITS Diff for testing.

---
+++ tests/drptestbones/diff.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from astropy.extern import six
+from astropy.io.fits.diff import FITSDiff
+
+from astropy.extern.six.moves import StringIO
+
+__all__ = ['fits_osiris_allclose']
+
+def fits_osiris_allclose(a, b):
+    """Assert that two OSIRIS fits files are close."""
+    
+    report = StringIO()
+    diff = FITSDiff(
+        a, b,
+        ignore_keywords=["COMMENT"],
+        ignore_comments=["SIMPLE"],
+        ignore_fields=[],
+        ignore_blanks=True,
+        ignore_blank_cards=True)
+    diff.report(fileobj=report)
+    assert diff.identical, report.getvalue()
