8d82f3f4e49367035a89f3d75d191dd8530f18f2
Tuan Do <followthesheep@gmail.com>
2017-07-21 08:15:24 -1000

    modified the lenslet mapping to account for row start differences

--- testing_scripts/trace_spectra/extract_lenslet_map.py
+++ testing_scripts/trace_spectra/extract_lenslet_map.py
@@ -16,14 +16,18 @@ def extract_kbb_map():
                         parse_cols='C:U',skiprows=4)
     tab = tab[:64]
     t2 = np.array(tab,dtype=int)
-
+#    t2[:,0] = t2[:,0]+2
+    
     # save the lenslet mapping
-    np.savetxt('kn3_lenslet_mapping.txt',t2,delimiter=' ',fmt='%i')
+    np.savetxt('kbb_2016_lenslet_mapping.txt',t2,delimiter=' ',fmt='%i')
 
     # we can also unravel the lenslet mapping
     s = np.shape(t2)
-    output = open('kn3_slice_to_lenslet.txt','w')
+    output = open('kbb_2016_slice_to_lenslet.txt','w')
+    print(s)
     for i in xrange(s[0]):
         for j in xrange(s[1]):
-            output.write('%i %i %i\n' % (t2[i,j],i,j))
+            row = t2[i,0]+2  # the rows start at 2
+            col = j
+            output.write('%i %i %i\n' % (t2[i,j],row,col))
     output.close()
